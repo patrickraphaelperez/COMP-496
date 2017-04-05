dbef7b2dc25f5176d54421b39fef835069ec372a
IrvinAlo <irvin.alo.206@my.csun.edu>
2017-04-05 13:58:57 -0700

    EDF and Own Creation in progresss

--- Project 2 - Scheduling/src/JobScheduler.java
+++ Project 2 - Scheduling/src/JobScheduler.java
@@ -11,7 +11,7 @@ import java.util.PriorityQueue;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class jobScheduler
+public class JobScheduler
 {
   private int nJobs;
   private Job[] jobs;   
@@ -35,11 +35,7 @@ public class jobScheduler
 	  }
   }
     
-  public Schedule scheduler(Job[] arr) {
-	  //Scheduler object
-	  
-	  return null;
-  }
+
   //Brute force. Try all n! orderings. Return the schedule with the most profit
   public Schedule bruteForceSolution()
   {
@@ -49,34 +45,33 @@ public class jobScheduler
   }
 
  
-  public Schedule makeScheduleEDF()  
+  public Schedule makeScheduleEDF()  //IRVIN
   //earliest deadline first schedule. Schedule items contributing 0 to total profit last
   {
+	  Schedule earliestdeadline = new Schedule();
 	  
 	  //Sort tasks by earliest deadline--------------- (insertion sort)
+	  
 	  Job temp;
-	  for (int i = 0; i >= nJobs; i++){
-		  for (int j = i; j > 0; j--){
-			  
-			  if (jobs[i].getDeadline() < jobs[i+1].getDeadline()) {
+	  for (int i = 0; i < nJobs; i++){
+			  int j = i;
+			  while (j > 0 && jobs[j-1].getDeadline() > jobs[j].getDeadline()) {
 				  
 				  temp = jobs[j];
 				  jobs[j] = jobs[j-1];
 				  jobs[j-1] = temp;
+				  j--;
 			  
 			  }
-		  }
+		  
 	  }
 	  //Set schedule
-	  Schedule earliestdeadline = new Schedule();
-	  
-	  
-	  
-	  
-	  
-	  
-
-	return null;  
+	  for (int i = 0; i < nJobs; i++) {
+		  earliestdeadline.add(jobs[i]);
+	  }
+	  // unprofitable jobs added to the end of the list
+	  earliestdeadline.completeSchedule();
+	  return earliestdeadline;  
   }
 
   public Schedule makeScheduleSJF() //PAT
@@ -94,12 +89,37 @@ public class jobScheduler
   {
 	  Schedule highestProfitFirst = new Schedule();
 	  Comparator<Job> comparator = new hpfComparison();
-	  PriorityQueue<Job> queue = new PriorityQueue<Job>(nJobs, comparator)
-	return null; }
+	  PriorityQueue<Job> queue = new PriorityQueue<Job>(nJobs, comparator);
+	return null; 
+  }
 
- public Schedule newApproxSchedule() //Your own creation. Must be <= O(n3)
+ public Schedule newApproxSchedule() //Your own creation. Must be <= O(n3) //IRVIN
 {
-	return null;  }
+	 // Creation idea: Longest job first
+	 Schedule longestJobFirst = new Schedule();
+	//Sort tasks by earliest deadline--------------- (insertion sort)
+	  Job temp;
+	  for (int i = 0; i >= nJobs; i++){
+		  for (int j = i; j > 0; j--){
+			  
+			  if (jobs[i].getLength() > jobs[i+1].getLength()) {
+				  
+				  temp = jobs[j];
+				  jobs[j] = jobs[j-1];
+				  jobs[j-1] = temp;
+			  
+			  }
+		  }
+	  }
+	  //Set schedule
+	  for (int i = 0; i < nJobs; i++) {
+		  longestJobFirst.add(jobs[i]);
+	  }
+	  // unprofitable jobs added to the end of the list
+	  longestJobFirst.completeSchedule();
+	  return longestJobFirst;
+	 
+	}
 
 }//end of JobScheduler class
 
@@ -159,11 +179,7 @@ class Schedule
      unprofitableSchedule = new ArrayList<Job>();
   }
   
-<<<<<<< HEAD
-  public void add(Job job)
-  {  
-	  schedule.add(job);
-=======
+
   //adding a job to the schedule (which is an array list)
   public void add(Job job)
   {  
@@ -203,7 +219,7 @@ class Schedule
 		  job.finish = job.start + job.length;
 		  this.schedule.add(job);
 	  }
->>>>>>> master
+
   }
     
  public int getProfit()
